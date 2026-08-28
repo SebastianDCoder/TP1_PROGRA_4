bdf11fa7f06f03da17a6c3c547569e06089eb709
Leandro <leandroserrano777@gmail.com>
2026-08-28 14:23:34 -0300

    Feat: Legajo automatico y constante

--- src/Ejecicio1/Empleado.java
+++ src/Ejecicio1/Empleado.java
@@ -2,17 +2,27 @@ package Ejecicio1;
 
 public class Empleado extends Persona{
 
-    private int legajo;
+    private final int legajo;
+    private static int legajoCounter = 999;
     private String puesto;
+
+    // Constructor
+
+
+    public Empleado(String puesto) {
+        legajoCounter++;
+        this.legajo = legajoCounter;
+        this.puesto = puesto;
+    }
 
     //Getters y Setters
     public int getLegajo() {
         return legajo;
     }
 
-    public void setLegajo(int legajo) {
-        this.legajo = legajo;
-    }
+    //public void setLegajo(int legajo) {
+    //    this.legajo = legajo;
+    //}
 
     public String getPuesto() {
         return puesto;
--- src/Ejecicio1/Principal.java
+++ src/Ejecicio1/Principal.java
@@ -2,6 +2,21 @@ package Ejecicio1;
 
 public class Principal {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo :D");
+
+        Empleado[] empleados = new Empleado[3];
+        empleados[0] = new Empleado("Desarrollador");
+        empleados[1] = new Empleado("Diseñador");
+        empleados[2] = new Empleado("Analista");
+
+        Empleado emple = new Empleado("Tester");
+
+
+        System.out.println("Empleados:");
+        for (Empleado e : empleados) {
+            System.out.println(e.toString());
+        }
+
+        System.out.println(emple.toString());
+
     }
 }
